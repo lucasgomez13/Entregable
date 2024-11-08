3620fe690eb7e41a20d37b6eef91cf179053b1cc
GÓMEZ FERNÁNDEZ Lucas Agustín <lagomez@correo.um.edu.uy>
2024-11-08 19:38:12 -0300

    correción

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,1 +1,6 @@
-from clases.Aventurero import Aventureros
+from entities.Aventurero import Aventureros
+from entities.Guerrero import Guerrero
+from entities.Mago import Mago
+from entities.Mascota import Mascota
+from entities.Ranger import Ranger
+from entities.Misiones import Misión
